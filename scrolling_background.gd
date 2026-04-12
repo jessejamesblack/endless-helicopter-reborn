9f54df7f1c8ada3a471ef8f6be7859d94d977857
Jesse-James Black <jessejamesblack19@gmail.com>
2026-04-12 01:51:01 -0400

    background fix

--- scrolling_background.gd
+++ scrolling_background.gd
@@ -5,3 +5,6 @@ extends Parallax2D
 func _ready() -> void:
     # Parallax2D has built-in autoscrolling! We just set the vector once.
     autoscroll = Vector2(-scroll_speed, 0)
+    
+    # Increase the number of times the background repeats to cover ultra-wide screens
+    repeat_times = 4
